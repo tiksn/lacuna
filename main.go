e31df366ab7fa9c1475474ca23c4f9c794655968
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-05-31 16:01:17 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,15 +1,38 @@
 package main
 
 import (
-	"os"
-	"log"
+	"fmt"
 	"github.com/urfave/cli"
+	"log"
+	"os"
 	// "github.com/moby/buildkit/frontend/dockerfile/parser"
 )
 
 var app = cli.NewApp()
 
+func info() {
+	app.Name = "lacuna"
+	app.Usage = "Docker base image version setter CLI"
+	app.Author = "Tigran TIKSN Torosyan"
+	app.Version = "1.0.0"
+}
+
+func commands() {
+	app.Commands = []cli.Command{
+		{
+			Name:    "set-base-image-tag",
+			Aliases: []string{"s"},
+			Usage:   "Sets base image tag",
+			Action: func(c *cli.Context) {
+				fmt.Printf("not implemented yet. Versions is %q", c.Args().First())
+			},
+		},
+	}
+}
+
 func main() {
+	info()
+	commands()
 	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
