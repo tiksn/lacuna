c84519aff806cac7aecfdde5e94463645de5927c
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-06-01 13:39:28 +0300

    Update commands.go

--- src/commands.go
+++ src/commands.go
@@ -2,9 +2,36 @@ package main
 
 import (
 	"fmt"
-	// "github.com/moby/buildkit/frontend/dockerfile/parser"
+	"log"
+	"os"
+
+	"github.com/moby/buildkit/frontend/dockerfile/parser"
 )
 
-func setBaseImageVersion(infile string, version string, outfile string) {
-	fmt.Printf("not implemented yet. Versions is %q %q %q", infile, version, outfile)
+func setBaseImageVersion(infile string, imageName string, versionNumber string, outfile string) {
+
+	var reader, err = os.Open(infile)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var result, err2 = parser.Parse(reader)
+	if err2 != nil {
+		log.Fatalln(err2)
+	}
+
+	setBaseImageAstVersion(result.AST, imageName, versionNumber)
+}
+
+func setBaseImageAstVersion(node *parser.Node, imageName string, versionNumber string) {
+
+	for _, c := range node.Children {
+		setBaseImageAstVersion(c, imageName, versionNumber)
+	}
+	if node.Value == "from" {
+		fmt.Printf("X - %q", node.Value)
+		fmt.Printf("X - %q", node.Next.Value)
+		fmt.Println()
+	}
+
 }
