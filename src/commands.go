824d52a43cc1d96a6c6c8c464da1f22afa40338c
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-06-01 12:57:52 +0300

    Create commands.go

---
+++ src/commands.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	// "github.com/moby/buildkit/frontend/dockerfile/parser"
+)
+
+func setBaseImageVersion(infile string, version string, outfile string) {
+	fmt.Printf("not implemented yet. Versions is %q %q %q", infile, version, outfile)
+}
